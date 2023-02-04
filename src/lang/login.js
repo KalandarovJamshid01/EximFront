9d307a751076c94705a0002872aaba4221aae6eb
Jamshid <kalandarovjamshid01@gmail.com>
2023-02-04 19:28:12 +0500

    Okay login is ready/ bu yogi uzbek

--- src/lang/login.js
+++ src/lang/login.js
@@ -12,10 +12,11 @@ const words = {
       "Срок действия сертификата: 05.06.2020 - 05.06.2022",
     I_agree_with_the_public_offer: "Я согласен с публичной офертой",
   },
-  spanish: {
-    signIn: "Registrarse",
-    email: "Correo Electrónico",
-    password: "Contraseña",
-    remember: "Recuérdame",
+  uzbek: {
+    log_in: "Log in",
+    Login_with_EDS: "ERI bilan tizimga kirish",
+    Certificate_validity_period:
+      "Sertifikatning amal qilish muddati: 05.06.2020 - 05.06.2022",
+    I_agree_with_the_public_offer: "Men ommaviy ofertaga roziman",
   },
 };
