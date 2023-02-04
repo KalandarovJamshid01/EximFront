4ac765a6f8f7877e1c024250435bc1f3b022b061
mironshoh1603 <mironshohasadov1603@gmail.com>
2023-02-04 18:29:46 +0500

    finished languages russian

---
+++ src/lang/Notifications.js
@@ -0,0 +1,28 @@
+const words = {
+  english: {},
+  russian: {
+    Your_application_rejected:
+      "Ваша заявка №846246 была отклонена, пожалуйста, попробуйте снова",
+    Congratulations_successfully_file:
+      "Поздравляем, заявка №846246 прошла проверку успешно, вы можете загрузить готовый файл. Спасибо, что выбрали нас!",
+    Application_No_successfully_waiting:
+      "Заявка №23141 от 29.12.2022 в 14:40:32 была успешно проверена на наличие документов и передана декларанту для оценки стоимости работ. Пожалуйста, ожидайте",
+    Payment_confirmed_message:
+      "Оплата была подтверждена, начинаем процедуру декларирования",
+    Payment_was_successful: "Оплата прошла успешно!",
+    Payment_confirmed_procedure:
+      "Оплата была подтверждена, начинаем процедуру декларирования",
+    Invoice_accepted_by_customer_payment_pending:
+      "Счёт принят клиентом, ожидается оплата",
+    Congratulations_choosing_us:
+      "Поздравляем, заявка №846246 прошла проверку успешно, вы можете загрузить готовый файл. Спасибо, что выбрали нас",
+
+    Notifications: "Уведомления",
+  },
+  spanish: {
+    Your_application_was_rejected_please_try_again: "Registrarse",
+    email: "Correo Electrónico",
+    password: "Contraseña",
+    remember: "Recuérdame",
+  },
+};
