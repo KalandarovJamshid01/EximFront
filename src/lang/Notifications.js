df03b3b897279195cd245866f9dc3b8e7cbc0921
Jamshid <kalandarovjamshid01@gmail.com>
2023-02-05 11:02:31 +0500

    okay

--- src/lang/Notifications.js
+++ src/lang/Notifications.js
@@ -16,13 +16,28 @@ const words = {
       "Счёт принят клиентом, ожидается оплата",
     Congratulations_choosing_us:
       "Поздравляем, заявка №846246 прошла проверку успешно, вы можете загрузить готовый файл. Спасибо, что выбрали нас",
-
     Notifications: "Уведомления",
+    wait: "Пожалуйста, ожидайте",
+    Clear_notify: "Очистить уведомления",
   },
-  spanish: {
-    Your_application_was_rejected_please_try_again: "Registrarse",
-    email: "Correo Electrónico",
-    password: "Contraseña",
-    remember: "Recuérdame",
+  uzbek: {
+    Your_application_rejected:
+      "№846246 -sonli so‘rovingiz rad etildi, qayta urinib ko‘ring",
+    Congratulations_successfully_file:
+      "Tabriklaymiz, №846246-sonli  so‘rov muvaffaqiyatli tekshirildi, siz tayyor faylni yuklashingiz mumkin. Bizni tanlaganingiz uchun tashakkur!",
+    Application_No_successfully_waiting:
+      "2022-yil 29-dekabr 14:40:32 da 23141-sonli so‘rov  hujjatlarning mavjudligi tekshirildi va ishlarning narxini baholash uchun deklarantga topshirildi.",
+    Payment_confirmed_message:
+      "To‘lov tasdiqlandi, deklaratsiyalash jarayonini boshlaymiz",
+    Payment_was_successful: "To‘lov muvaffaqiyatli o‘tdi!",
+    Payment_confirmed_procedure:
+      "To‘lov tasdiqlandi, deklaratsiyalash jarayonini boshlaymiz",
+    Invoice_accepted_by_customer_payment_pending:
+      "Hisob-faktura mijoz tomonidan qabul qilingan, to‘lov kutilmoqda",
+    Congratulations_choosing_us:
+      "Tabriklaymiz, №846246-sonli  so‘rov muvaffaqiyatli tekshirildi, siz tayyor faylni yuklashingiz mumkin. Bizni tanlaganingiz uchun tashakkur!",
+    Notifications: "Bildirishnoma",
+    wait: "Iltimos kuting.",
+    Clear_notify: "Bildirishnomalarni tozalash",
   },
 };
